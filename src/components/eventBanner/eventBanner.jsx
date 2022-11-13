eeb73d064f32589ad329f8c443c77e37c8f32014
HANSANGJUN <hsj11962@naver.com>
2022-11-13 21:13:12 +0900

    add cart styledcomponent

--- src/components/eventBanner/eventBanner.jsx
+++ src/components/eventBanner/eventBanner.jsx
@@ -1,20 +1,51 @@
-import styles from "./eventBanner.module.css";
+import styled from "styled-components";
 
 export const EventBanner = () => {
   return (
-    <article
-      className={styles.banner}
+    <Banner
       style={{
         backgroundImage: "url(/images/event.png",
         backgroundSize: "100%",
       }}
     >
-      <div className={styles.right}>
+      <div className="right">
         <img src="images/icon-swiper-2.svg" alt="right" />
       </div>
-      <div className={styles.left}>
+      <div className="left">
         <img src="images/icon-swiper-1.svg" alt="left" />
       </div>
-    </article>
+    </Banner>
   );
 };
+
+const Banner = styled.div`
+  position: relative;
+  width: 100%;
+  height: 500px;
+  background: #f2f2f2;
+  margin-bottom: 80px;
+
+  .right {
+    position: absolute;
+    transform: translateY(-50%);
+    top: 50%;
+    right: 5%;
+    background: transparent;
+  }
+
+  .right img {
+    background: transparent;
+  }
+
+  .left {
+    position: absolute;
+    top: 50%;
+    left: 5%;
+    transform: translateY(-50%);
+    background: transparent;
+  }
+
+  .left img {
+    background: transparent;
+  }
+`;
